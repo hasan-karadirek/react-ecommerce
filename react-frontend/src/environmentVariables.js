ef4e47d8bb9adbe6f90bd9d1269fa3ce71da7857
HackYourFuture <hackyourfuture@HackYouturesAir.home>
2023-10-11 02:16:13 +0200

    env var

--- react-frontend/src/environmentVariables.js
+++ react-frontend/src/environmentVariables.js
@@ -1,2 +1,2 @@
 export const apiServer = "http://16.16.202.102:5000";
-export const frontEndDomain = "https://incredible-syrniki-af1d2c.netlify.app";
+export const frontEndDomain = "http://incredible-syrniki-af1d2c.netlify.app";
